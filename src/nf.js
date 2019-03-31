a2c2d6fd17014f8431d125f6aa2ab688d5e95254
xi4oh4o <xi4oh4o@gmail.com>
2019-04-01 00:36:14 +0800

    feat(src/nf.js): Add restrictions and exception handling

--- src/nf.js
+++ src/nf.js
@@ -7,20 +7,40 @@ const os = require('os');
 
 exports.fetchToDestPath = function(urls) {
 
-  console.log(`Downloading urls: ${urls}`);
-
   return new Promise((resolve, reject) => {
+
     const destPath = path.join(os.tmpdir(), path.basename(urls))
     const file = fs.createWriteStream(destPath)
-    file.on('close', () => resolve(destPath))
-    file.on('error', reject)
+    file.on('close', () => {
+      console.log(`Downloading urls: ${urls}`);
+      resolve(destPath);
+    });
+    file.on('error', reject);
 
     function checkStatus(res) {
-        if (res.ok) { // res.status >= 200 && res.status < 300
-          return res;
-        } else {
-          return reject(res.statusText)
-        }
+
+      if (!res.ok) {
+        return reject(res.statusText);
+      }
+
+      let fileType = res.headers.get('content-type');
+      let fileLength = res.headers.get('content-length');
+
+      const allowContentType = [
+        'video/mp4',
+        'video/x-flv'
+        // @todo Should add more vidoe content type.
+      ]
+
+      if (!allowContentType.includes(fileType)) {
+        return reject('File type not supported');
+      }
+
+      if (fileLength > 51200000) {
+        return reject('Upload size exceeded limit');
+      }
+
+      return res;
     }
 
     fetch(urls)
