c0180d34386e59eda218b38929281b67f254567f
xi4oh4o <xi4oh4o@gmail.com>
2019-03-31 23:06:43 +0800

    feat(src/nf.js): Add HTTP request status checking

--- src/nf.js
+++ src/nf.js
@@ -6,17 +6,27 @@ const path = require('path');
 const os = require('os');
 
 exports.fetchToDestPath = function(urls) {
+
   console.log(`Downloading urls: ${urls}`);
-  
+
   return new Promise((resolve, reject) => {
     const destPath = path.join(os.tmpdir(), path.basename(urls))
     const file = fs.createWriteStream(destPath)
     file.on('close', () => resolve(destPath))
     file.on('error', reject)
 
+    function checkStatus(res) {
+        if (res.ok) { // res.status >= 200 && res.status < 300
+          return res;
+        } else {
+          return reject(res.statusText)
+        }
+    }
+
     fetch(urls)
+    .then(checkStatus)
     .then(res => {
-        res.body.pipe(file);
-    });
+      res.body.pipe(file);
+    }).catch(err => reject(err));
   })
 }
